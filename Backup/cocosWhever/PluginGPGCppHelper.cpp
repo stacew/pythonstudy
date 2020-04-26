f847a719bcb8b4473493f325da3d1febe487c255
stacew <chunrans4@naver.com>
2020-04-26 09:54:24 +0900

    git 조정

--- Backup/cocosWhever/PluginGPGCppHelper.cpp
+++
@@ -1,6 +0,0 @@
-﻿#include "Util/define.h"
-#ifdef PLUGIN_GPG
-#include "Plugingpg/SdkboxGooglePlayWrappedObjects.h"
-
-void sdkbox::GPGWrapper::NotifyToScripting(int id, const std::string& str_json) {}
-#endif
